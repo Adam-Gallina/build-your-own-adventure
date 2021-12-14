c022e2c67bed6553b108c0980a3135609ed6b6a7
Adam-Gallina <adamg461@gmail.com>
2021-12-13 20:24:29 -0800

    Created basic server functionality

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log('Test')
+var path = require('path')
+var express = require('express')
+var exphbs = require('express-handlebars')
+
+var app = express()
+var port = process.env.PORT || 2282;
+
+app.engine('handlebars', exphbs.engine({defaultLayout : 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get('/', function(req, res, next) {
+    res.status(200).render('home')
+})
+
+app.get('*', function(req, res) {
+    res.status(404).render('404')
+})
+
+app.listen(port, function() {
+    console.log('-- Server is listening on port ' + port + '!')
+})
