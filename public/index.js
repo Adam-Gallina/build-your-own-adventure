25888787db9d71058f85440a25eb8fe85bdddcb7
Adam-Gallina <adamg461@gmail.com>
2021-12-13 23:01:19 -0800

    Added basic cyoa functionality

--- public/index.js
+++ public/index.js
@@ -1,19 +1,42 @@
+// Test functions for proof of concept
+function LoadElems() {
+    var elems = {}
+
+    elems.text = document.getElementById("node-text")
+
+    var btns = document.querySelectorAll('.nodeBtn button')
+    for (i = 0; i < btns.length; i++) {
+        elems["btn" + i] = btns[i]
+        btns[i].addEventListener('click', getBtnCallback(btns[i].id))
+    }
+
+    return elems
+}
+
+function RenderPage(elems, text, btnData) {
+    elems.text.innerHTML = text
+    for (i = 0; i < btnData.length; i++) {
+        elems["btn" + i].id = btnData[i].btnId
+        elems["btn" + i].innerHTML = btnData[i].btnText
+    }
+}
+
 // @TODO: function is exposed to client, can change pages w/o interacting with buttons
 //      - could potentially change to a page outside of the current branch
 function getBtnCallback(btnId) {
     return function(event) {
-        console.log("Clicked: " + btnId)
+        var id = event.target.id
 
         var req = new XMLHttpRequest()
-        req.open('GET', '/byoa/' + btnId)
+        req.open('GET', '/byoa/' + id)
         req.addEventListener('load', function(event) {
-            console.log(event.target.status + ": " + event.target.response)
+            if (event.target.status != 404) {
+                var data = JSON.parse(event.target.responseText)
+                RenderPage(elems, data.text, data.buttons)
+            }
         })
         req.send()
     }
 }
 
-var btns = document.querySelectorAll('.nodeBtn button')
-for (i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', getBtnCallback(btns[i].id))
-}
+elems = LoadElems()
